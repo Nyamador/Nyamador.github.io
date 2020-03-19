77d0ab9187591f78447d53d90408655641771f2b
Nyamador <dselasi4u@gmail.com>
2020-03-19 10:53:29 +0000

    Mobile Heading fixed :v1

--- p/request.py
+++ p/request.py
@@ -2,11 +2,16 @@ import requests
 import json
 
 
-def create_mux_asset(token='6c2116a7-38b9-4ca2-9c7f-dedc8e023883' , key='Pu3P7NIjcpRfhdAryK5zQq3iT0GL7haP4vnCfoCCtAQ8Y0hK+2j/ODpO5mLzUo9GjB3b/75tVeF'):
-	video_url = 'http://movietrailers.apple.com/movies/wb/the-lego-ninjago-movie/the-lego-ninjago-movie-trailer-2_h720p.mov'
-	response = requests.post('https://api.mux.com/video/v1/assets', data = video_url , auth=('username', getpass()))
+def send_mail():
+
+	response = requests.post("https://api.mailgun.net/v3/sandbox1118746407a347a1b579f7a545e2f6c4.mailgun.org/messages", 
+	auth=('api', 'a7fb3059e68a03b1c1c9f0a12ac9d22b-9dfbeecd-aa8f8dc2'),
+	data = {"from": "mailgun@sandbox1118746407a347a1b579f7a545e2f6c4.mailgun.org",
+			"to": ["dselasi4u@gmail.com", "selanyamador@gmail.com"],
+			"subject": "Desmond Nyamador",
+			"text": "Testing some Mailgun awesomness through the api!"})
 
 	print(response.json())
 
 
-create_mux_asset()
+send_mail()
