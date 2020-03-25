13defcbf11cb10078d732cdf1ea5f0033b842b07
Desmond <48738520+Nyamador@users.noreply.github.com>
2020-03-25 06:21:42 +0000

    Update request.py

--- p/request.py
+++ p/request.py
@@ -5,8 +5,8 @@ import json
 def send_mail():
 
 	response = requests.post("https://api.mailgun.net/v3/sandbox1118746407a347a1b579f7a545e2f6c4.mailgun.org/messages", 
-	auth=('api', 'a7fb3059e68a03b1c1c9f0a12ac9d22b-9dfbeecd-aa8f8dc2'),
-	data = {"from": "mailgun@sandbox1118746407a347a1b579f7a545e2f6c4.mailgun.org",
+	auth=('api', ''),
+	data = {"from": "mailgun.mailgun.org",
 			"to": ["dselasi4u@gmail.com", "selanyamador@gmail.com"],
 			"subject": "Desmond Nyamador",
 			"text": "Testing some Mailgun awesomness through the api!"})
